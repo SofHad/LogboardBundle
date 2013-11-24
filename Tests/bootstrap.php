5dcc624a7ec1eea03a438dacc9e6f51c7c515d6d
Sofiane Haddag <sofiane.haddag@gmail.com>
2013-11-24 23:21:35 +0100

    Conf

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,11 +2,14 @@
 $sfAutoload = __DIR__ . '/../app/vendor/autoload.php';
 $bundleAutoload = __DIR__ . '/../vendor/autoload.php';
 
+require_once $sfAutoload;
+require_once $bundleAutoload;
 
-if (file_exists($sfAutoload)) {
-    $autoload = require_once $sfAutoload;
-} elseif (file_exists($bundleAutoload)) {
-    $autoload = require_once $bundleAutoload;
-} else {
-    throw new RuntimeException('Install dependencies to run test suite.');
-}
+//
+//if (file_exists($sfAutoload)) {
+//    $autoload = require_once $sfAutoload;
+//} elseif (file_exists($bundleAutoload)) {
+//    $autoload = require_once $bundleAutoload;
+//} else {
+//    throw new RuntimeException('Install dependencies to run test suite.');
+//}
