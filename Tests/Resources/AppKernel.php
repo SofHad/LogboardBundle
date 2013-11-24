18c8adf6611ac9477213c8358f0533958410e8cd
Sofiane Haddag <sofiane.haddag@gmail.com>
2013-11-24 16:55:48 +0100

    Add fixed travis

--- Tests/Resources/AppKernel.php
+++ Tests/Resources/AppKernel.php
@@ -10,7 +10,13 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle()
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            //new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            //new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            //new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
         );
 
         return $bundles;
@@ -18,6 +24,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-       // $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+         $loader->load(__DIR__.'/config.yml');
     }
 }
