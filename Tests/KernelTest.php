d7499d6ee60d6181605f6ba58fcc7f51e589cc05
Sofiane Haddag <sofiane.haddag@gmail.com>
2013-11-24 16:14:35 +0100

    Add fixed travis

--- Tests/KernelTest.php
+++ Tests/KernelTest.php
@@ -10,11 +10,11 @@
 
 namespace So\LogboardBundle\Tests;
 
-$file = __DIR__ . '/../../app/AppKernel.php';
+$file = __DIR__ . '/../vendor/app/AppKernel.php';
 
 if (!file_exists($file)) {
 
-    if ($handle = opendir(dirname(dirname(__DIR__)))) {
+    if ($handle = opendir( __DIR__ . '/../vendor/')) {
         echo "Gestionnaire du dossier : $handle\n";
         echo "Entrées :\n";
 
