67c2b12e019271f35622073e36e1bd0baf60d561
Sofiane Haddag <sofiane.haddag@gmail.com>
2013-11-25 00:06:25 +0100

    Conf

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,21 +1,6 @@
 <?php
-$sfAutoload = __DIR__ . '/../app/vendor/autoload.php';
+
 $bundleAutoload = __DIR__ . '/../vendor/autoload.php';
 
-$autoload = require_once $sfAutoload;
+$autoload = require_once $bundleAutoload;
 
-$loader->add('Vendor_', '/');
-
-// look for Vendor\Package classes in this path:
-$loader->add('So', __DIR__ . '/../../');
-
-$loader->register();
-
-//
-//if (file_exists($sfAutoload)) {
-//    $autoload = require_once $sfAutoload;
-//} elseif (file_exists($bundleAutoload)) {
-//    $autoload = require_once $bundleAutoload;
-//} else {
-//    throw new RuntimeException('Install dependencies to run test suite.');
-//}
