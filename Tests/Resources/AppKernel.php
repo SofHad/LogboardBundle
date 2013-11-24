8cbf09b6b8305388a2103c32f6e072f30781a951
Sofiane Haddag <sofiane.haddag@gmail.com>
2013-11-24 16:48:43 +0100

    Add fixed travis

--- Tests/Resources/AppKernel.php
+++ Tests/Resources/AppKernel.php
@@ -10,13 +10,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new \Symfony\Bundle\TwigBundle\TwigBundle(),
-            new \Symfony\Bundle\MonologBundle\MonologBundle(),
-            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle()
         );
 
         return $bundles;
