08e08cdfc5734592a8155986046dbe94a042e044
Sofiane Haddag <sofiane.haddag@gmail.com>
2013-11-24 23:04:31 +0100

    Conf

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-$file = __DIR__.'/../vendor/autoload.php';
+$file = __DIR__.'/../app/vendor/autoload.php';
 
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite.');
